ac467321daea550d69294279d7659c99f06d7638
Rahul Nayanegali <rahulnayanegali@gmail.com>
2020-11-21 08:01:31 +0530

    phase1

---
+++ src/projects/ProjectsList.js
@@ -0,0 +1,67 @@
+export const ProjectsList = [
+    {
+        repoName: 'react-socialapp',
+        description: 'A social media app, where we can post, comment, delete, like and dislike a tweet.',
+        liveUrl: 'https://twitter-clone-5df18.web.app/',
+        hashTags:['react','redux', 'redux-thunk', 'material-ui'],
+
+    },
+    {
+        repoName: 'declutterJsBlog',
+        description: 'This app is built using HTML, CSS and Plane Javascript',
+        liveUrl: 'https://declutterjs.netlify.app/',
+        hashTags:['javascript', 'html', 'css'],
+    }
+]
+    /*
+    {
+        repoName: '',
+        description: '',
+        liveUrl: '',
+        hashTags:[],
+    }
+    */
+   /*
+   <div class="max-w-sm rounded overflow-hidden shadow-lg">
+                <img class="block h-auto w-full" src={sampleImage} alt="Sunset in the mountains" />
+                <div class="px-6 py-4">
+                    <div class="font-bold text-xl mb-2">The Coldest Sunset</div>
+                    <p class="text-gray-700 text-base">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
+                    </p>
+                </div>
+                <div class="px-6 pt-4 pb-2">
+                    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
+                    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
+                    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
+                </div>
+            </div>
+            <div class="max-w-sm rounded overflow-hidden shadow-lg">
+                <img class="block h-auto w-full" src={sampleImage} alt="Sunset in the mountains" />
+                <div class="px-6 py-4">
+                    <div class="font-bold text-xl mb-2">The Coldest Sunset</div>
+                    <p class="text-gray-700 text-base">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
+                    </p>
+                </div>
+                <div class="px-6 pt-4 pb-2">
+                    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
+                    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
+                    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
+                </div>
+            </div><div class="max-w-sm rounded overflow-hidden shadow-lg">
+                <img class="block h-auto w-full" src={sampleImage} alt="Sunset in the mountains" />
+                <div class="px-6 py-4">
+                    <div class="font-bold text-xl mb-2">The Coldest Sunset</div>
+                    <p class="text-gray-700 text-base">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
+                    </p>
+                </div>
+                <div class="px-6 pt-4 pb-2">
+                    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
+                    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
+                    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
+                </div>
+            </div>
+*/
+    
--- src/projects/project.js
+++ src/projects/project.js
@@ -1,50 +1,51 @@
 import React, { Fragment } from 'react';
+import {ProjectsList} from './ProjectsList';
 import sampleImage from '../images/pexels-pixabay-270637.jpg'
+
+const Projects = () => {ProjectsList.map(project => {
+    return (
+console.log(project)
+    )
+    
+} )}
 export default function project() {
+  
     return (
         <Fragment>
-            <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
-            <div class="max-w-sm rounded overflow-hidden shadow-lg">
-                <img class="block h-auto w-full" src={sampleImage} alt="Sunset in the mountains" />
-                <div class="px-6 py-4">
-                    <div class="font-bold text-xl mb-2">The Coldest Sunset</div>
-                    <p class="text-gray-700 text-base">
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
-                    </p>
-                </div>
-                <div class="px-6 pt-4 pb-2">
-                    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
-                    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
-                    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
-                </div>
-            </div>
-            <div class="max-w-sm rounded overflow-hidden shadow-lg">
-                <img class="block h-auto w-full" src={sampleImage} alt="Sunset in the mountains" />
-                <div class="px-6 py-4">
-                    <div class="font-bold text-xl mb-2">The Coldest Sunset</div>
-                    <p class="text-gray-700 text-base">
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
-                    </p>
-                </div>
-                <div class="px-6 pt-4 pb-2">
-                    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
-                    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
-                    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
-                </div>
-            </div><div class="max-w-sm rounded overflow-hidden shadow-lg">
-                <img class="block h-auto w-full" src={sampleImage} alt="Sunset in the mountains" />
-                <div class="px-6 py-4">
-                    <div class="font-bold text-xl mb-2">The Coldest Sunset</div>
-                    <p class="text-gray-700 text-base">
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
-                    </p>
-                </div>
-                <div class="px-6 pt-4 pb-2">
-                    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
-                    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
-                    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
-                </div>
-            </div>
+            <h1 className="font-semibold text-3xl tracking-tight mb-4 px-6 py-4">Projects</h1>
+            <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3 mx-auto">
+            {
+                ProjectsList.map(project => {
+                    // let thumbnail = `http://free.pagepeeker.com/v2/thumbs.php?size=m&url=${project.liveUrl}`
+                    let thumbnail = `https://www.google.com/s2/favicons?domain=${project.liveUrl}`
+                    let repoUrl = `https://www.github.com/rahulnayanegali/${project.repoName}`
+                    return (
+                        <div class="max-w-sm rounded overflow-hidden shadow-lg">
+                            {/* <img class="block h-auto w-full" src={thumbnail} alt="Sunset in the mountains" /> */}
+                            <div class="px-6 py-4">
+                                <div class="font-bold text-xl mb-2"><a href={repoUrl} target="_blank">{project.repoName}</a></div>
+                                <p class="text-gray-700 text-base">
+                                    {project.description}
+                                </p>
+                                {/* <p className="text-gray-700 text-base"> */}
+                                    <a className="font-bold hover:no-underline" target="_blank" href={project.liveUrl}>{project.liveUrl}</a>
+                                {/* </p> */}
+                            </div>
+                            <div class="px-6 pt-4 pb-2">
+                                {project.hashTags.map(hashTag => {
+                                    return(
+                                    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#{hashTag}</span>
+                                    )
+                                })}
+                                
+                                
+                            </div>
+                        </div>
+                    )
+                })
+            }
+            
+            
             </div>
             
             
